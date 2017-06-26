5ab9a7fab8c3d7166de81e92dc18671dbe871925
Michael Oles <mbo10@pitt.edu>
2017-06-25 22:41:49 -0400

    add exercise list template fix

--- app/src/main/java/edu/pitt/cs/cs1635/mbo10/swiftset/ExerciseSelector.java
+++ app/src/main/java/edu/pitt/cs/cs1635/mbo10/swiftset/ExerciseSelector.java
@@ -1,7 +1,11 @@
 package edu.pitt.cs.cs1635.mbo10.swiftset;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 import android.widget.ListView;
@@ -12,10 +16,13 @@ public class ExerciseSelector extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exercise_selector);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-        String[] searchResults = {"Bench Press", "Squat", "DeadLift"};
+        String[] searchResults = {"Bench Press", "Squat", "Deadlift"};
         ListAdapter la = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,searchResults);
         ListView exListView = (ListView) findViewById(R.id.exerciseList);
         exListView.setAdapter(la);
     }
+
 }
