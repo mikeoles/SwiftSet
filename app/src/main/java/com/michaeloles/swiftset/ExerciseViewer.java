2d542dd4539492ddf0bbc3f28434fac1456a0cf4
Michael Oles <mbo10@pitt.edu>
2017-09-26 14:24:48 -0400

    Fix youtube view loading

--- app/src/main/java/com/michaeloles/swiftset/ExerciseViewer.java
+++ app/src/main/java/com/michaeloles/swiftset/ExerciseViewer.java
@@ -16,14 +16,21 @@ import com.google.android.youtube.player.YouTubeBaseActivity;
         import com.google.android.youtube.player.YouTubePlayer.Provider;
         import com.google.android.youtube.player.YouTubePlayerView;
 
+import java.text.NumberFormat;
+import java.text.ParseException;
+
 public class ExerciseViewer extends YouTubeBaseActivity implements YouTubePlayer.OnInitializedListener {
 
     private static final int RECOVERY_REQUEST = 1;
     private YouTubePlayerView youTubeView;
+    private static String videoCode = "";
     private static String youtubeCode = "";
     private static int startTimeMillis = 0;
     private static String selectedExercise = "";
     private static boolean wasMusicPlaying = false;
+    private static boolean badUrl = false;
+    private static final String fullUrl = "youtube.com/watch?v=";
+    private static final String shortUrl = "youtu.be/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +38,7 @@ public class ExerciseViewer extends YouTubeBaseActivity implements YouTubePlayer
         setContentView(R.layout.activity_exercise_viewer);
 
         wasMusicPlaying = pauseMusic();
+        startTimeMillis = 0;
 
         //Get Url From Calling Activity
         Bundle extras = getIntent().getExtras();
@@ -39,34 +47,50 @@ public class ExerciseViewer extends YouTubeBaseActivity implements YouTubePlayer
         String selectedUrl = remaining.getUrlByExerciseName(selectedExercise);
 
         //seperate the youtube video code and time from the url
-        if(selectedUrl.toLowerCase().contains("youtu.be")){//different depending on youtube.com and youtu.be urls
-            youtubeCode = selectedUrl.substring(selectedUrl.lastIndexOf("/") + 1);
+        if(selectedUrl.toLowerCase().contains(fullUrl)){//different depending on youtube.com and youtu.be urls
+            youtubeCode = selectedUrl.substring(selectedUrl.lastIndexOf(fullUrl) + fullUrl.length());
+        }else if(selectedUrl.toLowerCase().contains(shortUrl)){
+            youtubeCode = selectedUrl.substring(selectedUrl.lastIndexOf(shortUrl) + shortUrl.length());
         }else{
-            youtubeCode = selectedUrl.substring(selectedUrl.lastIndexOf("=") + 1);
+            badUrl = true;
         }
 
-        if(youtubeCode.contains("&")){
-            String[] parts = youtubeCode.split("&");
-            youtubeCode = parts[0];//The code in the url the speifies the current video
-            String timecode = parts[1].toLowerCase();//The part of the url that specifices the start time
-            timecode = timecode.substring(2, timecode.length());//Remove the first part of the time url (t=)
+        //Find the video code from the url
+        //Ex: Url = https://www.youtube.com/watch?v=D5d_rkxPfuE&t=1m4s videoCode = D5d_rkxPfuE
+        int endOfVideoCode = findFirstSeperator(youtubeCode);
+        videoCode = youtubeCode.substring(0,endOfVideoCode);
 
-            if(!timecode.contains("m")&&!timecode.contains("s")){//timecode is just listed as an interger of seconds
-                startTimeMillis += Integer.parseInt(timecode) * 1000;
+        //If a specific time is designated in the video set the start time in milliseconds
+        if(youtubeCode.contains("t=") && endOfVideoCode<youtubeCode.length()) {
+            //removes the video code from the youtubeCode
+            youtubeCode = youtubeCode.substring(endOfVideoCode + 1, youtubeCode.length());
+            //Timecode is everything after t=
+            String timecode = youtubeCode.substring(youtubeCode.indexOf("t=")+2,youtubeCode.length());
+            //and everything before the first seperator
+            timecode = timecode.substring(0,findFirstSeperator(timecode));
+            //Ex: t=1m5s&index=2&list=WL&index=3 -> 1m5s
+
+            if (!timecode.contains("m") && !timecode.contains("s")) {//timecode is just listed as an interger of seconds
+                try {
+                    startTimeMillis += startTimeMillis += ((Number) NumberFormat.getInstance().parse(timecode)).intValue() * 1000;
+                } catch (ParseException e) {
+                    badUrl = true;
+                }
             }
-            if(timecode.contains("m")){//if both minutes and seconds are noted in url
-                parts = timecode.split("m");
+
+            if (timecode.contains("m")) {//m in url d
+                String[] parts = timecode.split("m");
                 startTimeMillis = Integer.parseInt(parts[0]) * 60000;//Convert the url minutes time to milliseconds
                 timecode = parts[1];
             }
-            if(timecode.contains("s")){
-                timecode = timecode.substring(0, timecode.length() - 1);//remove trailing s from string to get number of seconds
-                startTimeMillis += Integer.parseInt(timecode) * 1000;
-            }
-        }
 
-        if(youtubeCode.contains("?")){
-            youtubeCode = youtubeCode.substring(0,youtubeCode.indexOf("?"));
+            if (timecode.contains("s")) {
+                try {
+                    startTimeMillis += startTimeMillis += ((Number) NumberFormat.getInstance().parse(timecode)).intValue() * 1000;
+                } catch (ParseException e) {
+                    badUrl = true;
+                }
+            }
         }
 
         TextView t = (TextView) findViewById(R.id.exerciseTitle);
@@ -76,6 +100,24 @@ public class ExerciseViewer extends YouTubeBaseActivity implements YouTubePlayer
             youTubeView.initialize(Config.YOUTUBE_API_KEY, this);
         }
     }
+
+    //Finds the index of the first location of a seperator character in the URL
+    //Returns the end of the string if none are found
+    private int findFirstSeperator(String s) {
+        int endOfVideoCode = s.length();
+        if (s.contains("&")) {
+            endOfVideoCode = Math.min(endOfVideoCode, s.indexOf("&"));
+        }
+        if (s.contains("?")) {
+            endOfVideoCode = Math.min(endOfVideoCode, s.indexOf("?"));
+        }
+        if (s.contains("\n")) {
+            endOfVideoCode = Math.min(endOfVideoCode, s.indexOf("\n"));
+        }
+        //Code is after url and before the first seperator character (?/&)
+        return endOfVideoCode;
+    }
+
     //If music is playing in the background it pauses it so the youtube video can play
     //Returns if there was music playing so we know to resume it after leaving the view
     private boolean pauseMusic() {
@@ -93,8 +135,8 @@ public class ExerciseViewer extends YouTubeBaseActivity implements YouTubePlayer
     @Override
     public void onInitializationSuccess(Provider provider, YouTubePlayer player, boolean wasRestored) {
         if (!wasRestored) {
-            Log.v("olesy",youtubeCode + " " + Integer.toString(startTimeMillis));
-            player.cueVideo(youtubeCode, startTimeMillis);
+            Log.v("olesy",videoCode + " " + Integer.toString(startTimeMillis));
+            player.cueVideo(videoCode, startTimeMillis);
             player.play();
         }
     }
